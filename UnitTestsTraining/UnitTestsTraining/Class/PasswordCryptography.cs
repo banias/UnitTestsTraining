400aea274e1bcfd1ac02d9759c0c0a4117e75ca7
grzegorzb <grzegorz.banczak@medius-group.pl>
2015-03-23 11:38:45 +0100

    Initial commit

---
+++ UnitTestsTraining/UnitTestsTraining/Class/PasswordCryptography.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsTraining
+{
+    public class PasswordCryptograhy
+    {
+        private const string HashPrefix = "#¶µ•#‡";
+
+	public static bool IsPasswordHash(string text)
+	{
+	    return !string.IsNullOrEmpty(text) && text.StartsWith(HashPrefix) && text.Length == 50;
+	}
+
+        public string GenerateHash(string input)
+        {
+            var encoding = new UnicodeEncoding();
+            var passInBytes = encoding.GetBytes(input);
+            var sha = new SHA256Managed();
+            var hashInBytes = sha.ComputeHash(passInBytes);
+            return string.Format("{0}{1}", HashPrefix, Convert.ToBase64String(hashInBytes));
+        }
+
+    }
+}
